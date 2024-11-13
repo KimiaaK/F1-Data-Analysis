a7e8501c44350b45b211620e75697b4a75f0ad55
KimiaaK <keam96@ymail.com>
2024-11-13 01:05:32 -0500

    updated plots

--- plots.py
+++ plots.py
@@ -1,11 +1,7 @@
-import fastf1 as ff1
-from fastf1 import plotting
 from matplotlib import pyplot as plt
-from matplotlib.pyplot import figure
 from matplotlib.collections import LineCollection
 from matplotlib import cm
 import numpy as np
-import pandas as pd
 
 
 def speedDistance(telemetry):
